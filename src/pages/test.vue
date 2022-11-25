c67523eecd9056b32871496575707657458304a8
weekkkk <nikitanedelko9@gmail.com>
2022-11-25 21:27:14 +0200

    remove components

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,11 +1,9 @@
 <!-- @format -->
 
 <script setup lang="ts">
-import NcDateTable from '@/components/date-table/nc-date-table.vue';
 </script>
 
 <template>
-  <nc-date-table />
 </template>
 
 <style lang="scss" scoped></style>
